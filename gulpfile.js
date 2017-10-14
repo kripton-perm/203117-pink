921c62c11d5133976926de935aa96bf088185c29
kripton-perm <mixail1024070@mail.ru>
2017-10-14 11:57:19 +0500

    Автоматизировал минификацию изображений

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 var webp = require("gulp-webp");
 
@@ -41,4 +42,14 @@ gulp.task('webp', function () {
     return gulp.src('webp-src/*.jpg')
         .pipe(webp())
         .pipe(gulp.dest('webp-res'));
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
 });
